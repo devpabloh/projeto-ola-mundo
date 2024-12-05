1a17b0330396b93f66b4d228e1a347e0cc44a6e1
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-12-05 11:08:24 -0300

    arr

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,10 @@
+import Banner from "componentes/Banner";
+
 export default function SobreMim(){
     return(
-        <h1>Sobre Mim</h1>
+        <div>
+            <Banner/>
+            <h1>Sobre Mim</h1>
+        </div>
     )
 }
